b78c70e2692f8e5bef4c67e5b8081a894a79c22a
0429oonishi <oonishireon110@gmail.com>
2021-06-04 23:51:01 +0900

    MiddleButtonViewを作成

---
+++ StudyRecordApp/Views/MiddleButtonView.swift
@@ -0,0 +1,33 @@
+//
+//  MiddleButton.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/06/04.
+//
+
+import UIKit
+
+final class MiddleButtonView: UIView {
+
+    @IBOutlet weak var middleButton: UIButton!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadNib()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadNib()
+    }
+
+    private func loadNib() {
+        guard let view = UINib(nibName: String(describing: type(of: self)), bundle: nil).instantiate(withOwner: self, options: nil).first as? UIView else {
+            return
+        }
+        view.frame = self.bounds
+        view.backgroundColor = .red
+        self.addSubview(view)
+    }
+
+}
