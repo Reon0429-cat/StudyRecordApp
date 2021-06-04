814041b34bc7c6cb0b0d7b59db5b0e84093125df
0429oonishi <oonishireon110@gmail.com>
2021-06-04 18:01:55 +0900

    first commit

---
+++ StudyRecordApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/06/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
