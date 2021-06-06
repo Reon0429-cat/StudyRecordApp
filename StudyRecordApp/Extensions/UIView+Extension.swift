50bcd84562e67785ea86c34252e7912220f8ac75
0429oonishi <oonishireon110@gmail.com>
2021-06-06 13:36:29 +0900

    UIView+Extensionを記述

--- StudyRecordApp/Extensions/UIView+Extension.swift
+++ StudyRecordApp/Extensions/UIView+Extension.swift
@@ -21,22 +21,14 @@ extension UIView {
         switch position {
             case .top:
                 border.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: width)
-                border.backgroundColor = color.cgColor
-                self.layer.addSublayer(border)
             case .left:
                 border.frame = CGRect(x: 0, y: 0, width: width, height: self.frame.height)
-                border.backgroundColor = color.cgColor
-                self.layer.addSublayer(border)
             case .right:
-                print(self.frame.width)
-                
                 border.frame = CGRect(x: self.frame.width - width, y: 0, width: width, height: self.frame.height)
-                border.backgroundColor = color.cgColor
-                self.layer.addSublayer(border)
             case .bottom:
                 border.frame = CGRect(x: 0, y: self.frame.height - width, width: self.frame.width, height: width)
-                border.backgroundColor = color.cgColor
-                self.layer.addSublayer(border)
         }
+        border.backgroundColor = color.cgColor
+        self.layer.addSublayer(border)
     }
 }
