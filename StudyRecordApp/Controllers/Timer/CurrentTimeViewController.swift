2dedfdaf7bb79e27010d289fb6ff08a12e2f76be
0429oonishi <oonishireon110@gmail.com>
2021-06-10 00:37:55 +0900

    現在時刻をlabelに反映

--- StudyRecordApp/Controllers/Timer/CurrentTimeViewController.swift
+++ StudyRecordApp/Controllers/Timer/CurrentTimeViewController.swift
@@ -7,6 +7,15 @@
 
 import UIKit
 
+private struct TimeManager {
+    func current() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss"
+        dateFormatter.locale = Locale(identifier: "ja_JP")
+        return dateFormatter.string(from: Date())
+    }
+}
+
 final class CurrentTimeViewController: MyTabBarController {
     
     @IBOutlet private weak var currentTimeLabel: UILabel!
@@ -14,6 +23,21 @@ final class CurrentTimeViewController: MyTabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        currentTimeLabel.text = TimeManager().current()
+        scheduledTimer()
+        
+    }
+    
+    private func scheduledTimer() {
+        Timer.scheduledTimer(timeInterval: 1,
+                             target: self,
+                             selector: #selector(updateTimer),
+                             userInfo: nil,
+                             repeats: true)
+    }
+    
+    @objc private func updateTimer() {
+        currentTimeLabel.text = TimeManager().current()
     }
     
 }
