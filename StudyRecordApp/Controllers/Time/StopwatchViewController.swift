4cea6ddf0ce815f39c9d2dbecc5466273dc74afb
0429oonishi <oonishireon110@gmail.com>
2021-06-10 23:42:03 +0900

    ボタンを消去

--- StudyRecordApp/Controllers/Time/StopwatchViewController.swift
+++ StudyRecordApp/Controllers/Time/StopwatchViewController.swift
@@ -22,14 +22,24 @@ final class StopwatchViewController: MyTabBarController {
     
     @IBOutlet private weak var timeLabel: UILabel!
     @IBOutlet private weak var controlTimeButton: UIButton!
+    @IBOutlet private weak var stackView: UIStackView!
     
-    private var state: TimerState = .stopped
     private var timer: Timer?
-    private var millisecond: Int { Int(timerCount * 100) % 100 }
-    private var second: Int { Int(timerCount) % 60 }
+    private var state: TimerState = .stopped
     private var minutes: Int { Int(timerCount / 60) }
+    private var second: Int { Int(timerCount) % 60 }
+    private var millisecond: Int { Int(timerCount * 100) % 100 }
+    private var isOverTime = false
     private var timerCount: CGFloat = 0 {
         didSet {
+            if timerCount < 0 {
+                timerCount = 0
+            }
+            if timerCount > 5999 {
+                timer?.invalidate()
+                hideControlTimeButton(true)
+                isOverTime = true
+            }
             timeLabel.text = String(format: "%02d:%02d.%02d",
                                     minutes, second, millisecond)
         }
@@ -46,27 +56,42 @@ final class StopwatchViewController: MyTabBarController {
         switch state {
             case .started:
                 timer?.invalidate()
-                controlTimeButton.setTitle("スタート")
             case .stopped:
-                timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { _ in
-                    self.timerCount += 0.01
+                timer = Timer.scheduledTimer(withTimeInterval: 0.01,
+                                             repeats: true) { [weak self] _ in
+                    self?.timerCount += 0.01
                 }
-                controlTimeButton.setTitle("ストップ")
         }
         state.toggle()
+        updateUI()
     }
     
     @IBAction private func resetButtonDidTapped(_ sender: Any) {
         timerCount = 0
         timer?.invalidate()
-        controlTimeButton.setTitle("スタート")
         state = .stopped
+        if isOverTime {
+            hideControlTimeButton(false)
+            isOverTime = false
+        }
+        updateUI()
     }
     
-    @IBAction private func advanceTimeButtonDidTapped(_ sender: Any) {
+    private func hideControlTimeButton(_ isHidden: Bool) {
+        stackView.arrangedSubviews[0].isHidden = isHidden
     }
     
-    @IBAction private func backTimeButtonDidTapped(_ sender: Any) {
+    private func updateUI() {
+        controlTimeButton.setTitle(
+            {
+                switch state {
+                    case .started: return "ストップ"
+                    case .stopped: return "スタート"
+                }
+            }()
+        )
     }
     
 }
+
+
