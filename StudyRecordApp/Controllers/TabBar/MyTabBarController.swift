2e1a75626e3b86a827f379df129de853edb09f0b
0429oonishi <oonishireon110@gmail.com>
2021-07-17 19:18:53 +0900

    カスタムTabBar追加

---
+++ StudyRecordApp/Controllers/TabBar/MyTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  MyTabBarController.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/07/17.
+//
+
+import UIKit
+
+class MyTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
