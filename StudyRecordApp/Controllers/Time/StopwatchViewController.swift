20795d9e2215c3569f94bbd22bfe9c9a38270a17
0429oonishi <oonishireon110@gmail.com>
2021-07-30 08:28:10 +0900

    StopwatchVCをリファクタリング

--- StudyRecordApp/Controllers/Time/StopwatchViewController.swift
+++ StudyRecordApp/Controllers/Time/StopwatchViewController.swift
@@ -7,13 +7,16 @@
 
 import UIKit
 
+// MARK: - ToDo ストップウォッチの限界時間を長くする
+// MARK: - ToDo バックグラウンドでも処理が行われるようにする
+
 private enum TimerState {
-    case started
-    case stopped
+    case played
+    case paused
     mutating func toggle() {
         switch self {
-            case .started: self = .stopped
-            case .stopped: self = .started
+            case .played: self = .paused
+            case .paused: self = .played
         }
     }
 }
@@ -22,10 +25,11 @@ final class StopwatchViewController: UIViewController {
     
     @IBOutlet private weak var timeLabel: UILabel!
     @IBOutlet private weak var controlTimeButton: UIButton!
+    @IBOutlet private weak var resetTimeButton: UIButton!
     @IBOutlet private weak var stackView: UIStackView!
     
     private var timer: Timer?
-    private var state: TimerState = .stopped
+    private var state: TimerState = .paused
     private var minutes: Int { Int(timerCount / 60) }
     private var second: Int { Int(timerCount) % 60 }
     private var millisecond: Int { Int(timerCount * 100) % 100 }
@@ -37,7 +41,7 @@ final class StopwatchViewController: UIViewController {
             }
             if timerCount > 5999 {
                 timer?.invalidate()
-                hideControlTimeButton(true)
+                controlTimeButton.isHidden = true
                 isOverTime = true
             }
             timeLabel.text = String(format: "%02d:%02d.%02d",
@@ -49,14 +53,16 @@ final class StopwatchViewController: UIViewController {
         super.viewDidLoad()
         
         timeLabel.font = .monospacedDigitSystemFont(ofSize: 40, weight: .regular)
+        controlTimeButton.layer.cornerRadius = controlTimeButton.frame.height / 2
+        resetTimeButton.layer.cornerRadius = resetTimeButton.frame.height / 2
         
     }
     
     @IBAction private func controlTimeButtonDidTapped(_ sender: Any) {
         switch state {
-            case .started:
+            case .played:
                 timer?.invalidate()
-            case .stopped:
+            case .paused:
                 timer = Timer.scheduledTimer(withTimeInterval: 0.01,
                                              repeats: true) { [weak self] _ in
                     self?.timerCount += 0.01
@@ -69,27 +75,22 @@ final class StopwatchViewController: UIViewController {
     @IBAction private func resetButtonDidTapped(_ sender: Any) {
         timerCount = 0
         timer?.invalidate()
-        state = .stopped
+        state = .paused
         if isOverTime {
-            hideControlTimeButton(false)
+            controlTimeButton.isHidden = false
             isOverTime = false
         }
         updateUI()
     }
     
-    private func hideControlTimeButton(_ isHidden: Bool) {
-        stackView.arrangedSubviews[0].isHidden = isHidden
-    }
-    
     private func updateUI() {
-        controlTimeButton.setTitle(
-            {
-                switch state {
-                    case .started: return "ストップ"
-                    case .stopped: return "スタート"
-                }
-            }()
-        )
+        controlTimeButton.setImage({
+            switch state {
+                case .played:
+                    return UIImage(systemName: "pause.fill")!
+                case .paused:
+                    return UIImage(systemName: "play.fill")!
+            }
+        }())
     }
-    
 }
