39655082a5e0b66c86e635a06bebe0297fcf117d
0429oonishi <oonishireon110@gmail.com>
2021-07-31 12:19:10 +0900

    StudyRecordの追加セル実装

--- StudyRecordApp/Views/Cells/StudyRecord/StudyRecordTitleTableViewCell.swift
+++ StudyRecordApp/Views/Cells/StudyRecord/StudyRecordTitleTableViewCell.swift
@@ -7,17 +7,12 @@
 
 import UIKit
 
-class StudyRecordTitleTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+final class StudyRecordTitleTableViewCell: UITableViewCell {
+    
+    var onTapEvent: (() -> Void)?
+    
+    func configure(onTapEvent: @escaping () -> Void) {
+        self.onTapEvent = onTapEvent
     }
     
 }
