edff033f100f18f8bca857bf25edce42cc241526
0429oonishi <oonishireon110@gmail.com>
2021-08-01 07:52:32 +0900

    GraphColorTileView実装

---
+++ StudyRecordApp/Views/Xibs/GraphColorTileView.swift
@@ -0,0 +1,67 @@
+//
+//  GraphColorTileView.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/07/31.
+//
+
+import UIKit
+
+protocol GraphColorTileViewDelegate: AnyObject {
+    func tileViewDidTapped(selectedView: UIView)
+}
+
+final class GraphColorTileView: UIView {
+    
+    @IBOutlet private weak var tileStackView: UIStackView!
+    
+    private var tileStackViews: [UIStackView] {
+        tileStackView.arrangedSubviews.map { $0 as! UIStackView }
+    }
+    weak var delegate: GraphColorTileViewDelegate?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        loadNib()
+        setupTileViews()
+
+    }
+    
+    private func setupTileViews() {
+        tileStackViews
+            .enumerated()
+            .forEach { verticalCount, stackView in
+                stackView.arrangedSubviews
+                    .map { $0 as! TileView }
+                    .enumerated()
+                    .forEach { horizontalCount, tileView in
+                        tileView.delegate = self
+                        let alpha = CGFloat(100 - 10 * horizontalCount)
+                        let color = TileColorType(rawValue: verticalCount)?.loadColor(alpha: alpha)
+                        tileView.backgroundColor = color
+                    }
+            }
+    }
+    
+    private func loadNib() {
+        guard let view = UINib(nibName: String(describing: type(of: self)),
+                               bundle: nil
+        ).instantiate(withOwner: self,
+                      options: nil)
+        .first as? UIView else {
+            return
+        }
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+    
+}
+
+extension GraphColorTileView: TileViewDelegate {
+    
+    func tileViewDidTapped(selectedView: UIView) {
+        delegate?.tileViewDidTapped(selectedView: selectedView)
+    }
+    
+}
