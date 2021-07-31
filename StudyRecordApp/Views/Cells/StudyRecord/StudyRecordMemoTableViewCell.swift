34ef9993581ca8c2d921a5e32db8a3b51b31b5be
0429oonishi <oonishireon110@gmail.com>
2021-07-31 13:08:41 +0900

    StudyRecordMemoCell作成

---
+++ StudyRecordApp/Views/Cells/StudyRecord/StudyRecordMemoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  StudyRecordMemoTableViewCell.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/07/31.
+//
+
+import UIKit
+
+class StudyRecordMemoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
