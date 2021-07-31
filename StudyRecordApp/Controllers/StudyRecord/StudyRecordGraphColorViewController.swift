4239603d8ef1641618d9dc199c48b5f493da5b8c
0429oonishi <oonishireon110@gmail.com>
2021-08-01 07:52:00 +0900

    StudyRecordGraphColorViewControllerを実装

---
+++ StudyRecordApp/Controllers/StudyRecord/StudyRecordGraphColorViewController.swift
@@ -0,0 +1,48 @@
+//
+//  StudyRecordGraphColorViewController.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/07/31.
+//
+
+import UIKit
+
+final class StudyRecordGraphColorViewController: UIViewController {
+    
+    @IBOutlet private weak var graphColorView: UIView! {
+        didSet { graphColorView.layer.cornerRadius = 10 }
+    }
+    @IBOutlet private weak var graphColorTileView: GraphColorTileView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        graphColorTileView.delegate = self
+        
+    }
+    
+    @IBAction private func saveButtonDidTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction private func dismissButtonDidTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    static func instantiate() -> StudyRecordGraphColorViewController {
+        let storyboard = UIStoryboard(name: "StudyRecordGraphColor", bundle: nil)
+        let studyRecordGraphColorVC = storyboard.instantiateViewController(
+            identifier: String(describing: self)
+        ) as! StudyRecordGraphColorViewController
+        return studyRecordGraphColorVC
+    }
+    
+}
+
+extension StudyRecordGraphColorViewController: GraphColorTileViewDelegate {
+    
+    func tileViewDidTapped(selectedView: UIView) {
+        print(#function)
+    }
+    
+}
