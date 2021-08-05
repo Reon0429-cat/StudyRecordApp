f1b6f6839bcb3bc0e92452f9a287d5573b6cd762
0429oonishi <oonishireon110@gmail.com>
2021-08-05 11:08:56 +0900

    階層変更

---
+++ StudyRecordApp/Models/StudyRecord/Record.swift
@@ -0,0 +1,20 @@
+//
+//  Record.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/06/05.
+//
+
+import RealmSwift
+
+final class Record: Object {
+    @objc dynamic var title: String = ""
+    @objc dynamic var time: Time? = Time()
+    @objc dynamic var expanded: Bool = false
+    @objc dynamic var memo: String = ""
+}
+
+final class Time: Object {
+    @objc dynamic var today: Int = 0
+    @objc dynamic var total: Int = 0
+}
