d6ae0738deb075894f02fb3aba0137f819a8c674
0429oonishi <oonishireon110@gmail.com>
2021-08-05 14:46:40 +0900

    リファクタリング

--- StudyRecordApp/Models/StudyRecord/Repositories/RecordRepository.swift
+++ StudyRecordApp/Models/StudyRecord/Repositories/RecordRepository.swift
@@ -1,11 +1,11 @@
 //
-//  RealmRecordRepository.swift
+//  RecordRepository.swift
 //  StudyRecordApp
 //
 //  Created by 大西玲音 on 2021/08/05.
 //
 
-import RealmSwift
+import Foundation
 
 protocol RecordRepositoryProtocol {
     func create(record: Record)
--- StudyRecordApp/Models/StudyRecord/UseCases/RecordUseCase.swift
+++ StudyRecordApp/Models/StudyRecord/UseCases/RecordUseCase.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+final class RecordUseCase {
+    
+    private let repository: RecordRepositoryProtocol
+    init(repository: RecordRepositoryProtocol) {
+        self.repository = repository
+    }
+    
+    var records: [Record] {
+        return repository.readAll()
+    }
+    
+    func changeOpeningAndClosing(at index: Int) {
+        let record = repository.read(at: index)
+        let title = record.title
+        let time = record.time
+        let isExpanded = !record.isExpanded
+        let memo = record.memo
+        let newRecord = Record(title: title, time: time, isExpanded: isExpanded, memo: memo)
+        repository.update(record: newRecord, at: index)
+    }
+    
+    func save(record: Record) {
+        repository.create(record: record)
+    }
+    
+}
