b75a66a1330e0bb90a4fa3b5a5329dd53198d67c
0429oonishi <oonishireon110@gmail.com>
2021-08-07 10:51:34 +0900

    StudyRecordSectionViewリファクタリング

--- StudyRecordApp/Views/Cells/StudyRecord/StudyRecordSectionView.swift
+++ StudyRecordApp/Views/Cells/StudyRecord/StudyRecordSectionView.swift
@@ -7,6 +7,23 @@
 
 import UIKit
 
+private enum MemoState {
+    case expanded
+    case shrinked
+    var title: String {
+        switch self {
+            case .expanded: return "▲ メモ"
+            case .shrinked: return "▼ メモ"
+        }
+    }
+    mutating func toggle() {
+        switch self {
+            case .expanded: self = .shrinked
+            case .shrinked: self = .expanded
+        }
+    }
+}
+
 protocol StudyRecordSectionViewDelegate: AnyObject {
     func baseViewDidTapped()
 }
@@ -20,42 +37,73 @@ final class StudyRecordSectionView: UITableViewHeaderFooterView {
     @IBOutlet private weak var totalStudyTimeLabel: UILabel!
     
     var didClickedButton: (() -> Void)?
-    private var isExpanded = false
     var delegate: StudyRecordSectionViewDelegate?
+    private var memoState: MemoState = .shrinked
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        baseView.layer.cornerRadius = 10
-        let tapGR = UITapGestureRecognizer(target: self,
-                                           action: #selector(baseViewDidTapped))
-        baseView.addGestureRecognizer(tapGR)
+        setupBaseView()
         
     }
     
     @IBAction private func memoButtonDidTapped(_ sender: Any) {
-        isExpanded.toggle()
-        memoButton.setTitle("\(isExpanded ? "▼" : "▲") メモ")
+        memoState.toggle()
+        memoButton.setTitle(memoState.title)
         didClickedButton?()
     }
     
     func configure(record: Record,
                    didClickedButton: @escaping () -> Void) {
         self.didClickedButton = didClickedButton
+        setupTitleLabel(record: record)
+        setupTimeLabel(record: record)
+        setupMemoButton(record: record)
+    }
+    
+    @objc private func baseViewDidTapped() {
+        delegate?.baseViewDidTapped()
+    }
+    
+}
+
+// MARK: - setup
+private extension StudyRecordSectionView {
+    
+    func setupBaseView() {
+        baseView.layer.cornerRadius = 10
+        let tapGR = UITapGestureRecognizer(target: self,
+                                           action: #selector(baseViewDidTapped))
+        baseView.addGestureRecognizer(tapGR)
+    }
+    
+    func setupTitleLabel(record: Record) {
         titleLabel.text = record.title
-        todayStudyTimeLabel.text = "今日: \(record.time.today)分"
-        totalStudyTimeLabel.text = "合計: \(record.time.total)分"
-        isExpanded = record.isExpanded
-        memoButton.setTitle("\(isExpanded ? "▼" : "▲") メモ")
+    }
+    
+    func setupTimeLabel(record: Record) {
+        todayStudyTimeLabel.text = {
+            if record.time.today >= 60 {
+                return "今日: \(record.time.today / 60)時間"
+            }
+            return "今日: \(record.time.today)分"
+        }()
+        totalStudyTimeLabel.text = {
+            if record.time.total >= 60 {
+                return "合計: \(record.time.total / 60)時間"
+            }
+            return "合計: \(record.time.total)分"
+        }()
+    }
+    
+    func setupMemoButton(record: Record) {
+        memoState = record.isExpanded ? .expanded : .shrinked
+        memoButton.setTitle(memoState.title)
         if record.memo.isEmpty {
             memoButton.isHidden = true
         } else {
             memoButton.isHidden = false
         }
-    }
-    
-    @objc private func baseViewDidTapped() {
-        delegate?.baseViewDidTapped()
     }
     
 }
