85b9171de17eaeca8e9be6cfe035034f8a6697d3
0429oonishi <oonishireon110@gmail.com>
2021-08-08 15:59:31 +0900

    並び替え用のセルを作成

---
+++ StudyRecordApp/Views/Cells/StudyRecord/StudyRecordSortTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  StudyRecordSortTableViewCell.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/08/08.
+//
+
+import UIKit
+
+class StudyRecordSortTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
