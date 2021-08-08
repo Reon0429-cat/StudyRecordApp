8e801cba24e50796ae52c465deb6de93ce237f17
0429oonishi <oonishireon110@gmail.com>
2021-08-08 15:01:53 +0900

    並び替え用のファイルを作成

---
+++ StudyRecordApp/Controllers/StudyRecord/StudyRecordSortViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StudyRecordSortViewController.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/08/08.
+//
+
+import UIKit
+
+class StudyRecordSortViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
