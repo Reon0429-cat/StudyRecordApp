aa0ca33fb382a114db2ebe48cfe32d9cb8fb6e63
0429oonishi <oonishireon110@gmail.com>
2021-08-11 18:51:57 +0900

    編集画面で履歴をタップ時にピッカーに反映

--- StudyRecordApp/Controllers/StudyRecord/StudyRecordTimeRecordViewController.swift
+++ StudyRecordApp/Controllers/StudyRecord/StudyRecordTimeRecordViewController.swift
@@ -8,25 +8,26 @@
 import UIKit
 
 protocol StudyRecordTimeRecordVCDelegate: AnyObject {
-    func saveButtonDidTapped(hour: Int, minutes: Int, date: String)
+    func saveButtonDidTapped(history: History)
 }
+
+// MARK: - ToDo 履歴の並び替えと消去と編集を実装する
 
 final class StudyRecordTimeRecordViewController: UIViewController {
     
-    @IBOutlet private weak var datePicker: UIDatePicker!
     @IBOutlet private weak var pickerView: UIPickerView!
     
+    private let years = [Int](2020...2030)
+    private let months = [Int](1...12)
+    private let days = [Int](1...31)
     private let hours = [Int](0...23)
     private let minutes = [Int](0...59)
-    private var selectedDate = ""
-    private var selectedHour = 0
-    private var selectedMinutes = 0
+    var history: History?
     var delegate: StudyRecordTimeRecordVCDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupDatePicker()
         setupPickerView()
         
     }
@@ -36,11 +37,7 @@ final class StudyRecordTimeRecordViewController: UIViewController {
     }
     
     @IBAction private func saveButtonDidTapped(_ sender: Any) {
-        selectedDate = Convert().stringFrom(datePicker.date,
-                                            format: "yyyy年MM月dd日")
-        delegate?.saveButtonDidTapped(hour: selectedHour,
-                                      minutes: selectedMinutes,
-                                      date: selectedDate)
+        delegate?.saveButtonDidTapped(history: history!)
         dismiss(animated: true, completion: nil)
     }
     
@@ -57,16 +54,26 @@ final class StudyRecordTimeRecordViewController: UIViewController {
 // MARK: - setup
 private extension StudyRecordTimeRecordViewController {
     
-    func setupDatePicker() {
-        datePicker.datePickerMode = .date
-        if #available(iOS 13.4, *) {
-            datePicker.preferredDatePickerStyle = .wheels
-        }
-    }
-    
     func setupPickerView() {
         pickerView.delegate = self
         pickerView.dataSource = self
+        if history == nil {
+            let year = Int(Convert().stringFrom(Date(), format: "yyyy"))!
+            let month = Int(Convert().stringFrom(Date(), format: "M"))!
+            let day = Int(Convert().stringFrom(Date(), format: "d"))!
+            pickerView.selectRow(year - years.first!, inComponent: 0, animated: true)
+            pickerView.selectRow(month - months.first!, inComponent: 1, animated: true)
+            pickerView.selectRow(day - days.first!, inComponent: 2, animated: true)
+            pickerView.selectRow(0, inComponent: 3, animated: true)
+            pickerView.selectRow(0, inComponent: 4, animated: true)
+            history = History(year: year, month: month, day: day, hour: 0, minutes: 0)
+        } else {
+            pickerView.selectRow(history!.year - years.first!, inComponent: 0, animated: true)
+            pickerView.selectRow(history!.month - months.first!, inComponent: 1, animated: true)
+            pickerView.selectRow(history!.day - days.first!, inComponent: 2, animated: true)
+            pickerView.selectRow(history!.hour, inComponent: 3, animated: true)
+            pickerView.selectRow(history!.minutes, inComponent: 4, animated: true)
+        }
     }
     
 }
@@ -74,21 +81,14 @@ private extension StudyRecordTimeRecordViewController {
 extension StudyRecordTimeRecordViewController: UIPickerViewDelegate {
     
     func pickerView(_ pickerView: UIPickerView,
-                    titleForRow row: Int,
-                    forComponent component: Int) -> String? {
-        switch component {
-            case 0: return "\(hours[row])時間"
-            case 1: return "\(minutes[row])分"
-            default: fatalError()
-        }
-    }
-    
-    func pickerView(_ pickerView: UIPickerView,
                     didSelectRow row: Int,
                     inComponent component: Int) {
         switch component {
-            case 0: selectedHour = row
-            case 1: selectedMinutes = row
+            case 0: history?.year = row + years.first!
+            case 1: history?.month = row + months.first!
+            case 2: history?.day = row + days.first!
+            case 3: history?.hour = row
+            case 4: history?.minutes = row
             default: fatalError()
         }
     }
@@ -98,16 +98,55 @@ extension StudyRecordTimeRecordViewController: UIPickerViewDelegate {
 extension StudyRecordTimeRecordViewController: UIPickerViewDataSource {
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 2
+        return 5
     }
     
     func pickerView(_ pickerView: UIPickerView,
                     numberOfRowsInComponent component: Int) -> Int {
         switch component {
-            case 0: return hours.count
-            case 1: return minutes.count
+            case 0: return years.count
+            case 1: return months.count
+            case 2: return days.count
+            case 3: return hours.count
+            case 4: return minutes.count
             default: fatalError()
         }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView,
+                    viewForRow row: Int,
+                    forComponent component: Int,
+                    reusing view: UIView?) -> UIView {
+        let view = UIView()
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 16)
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.heightAnchor.constraint(equalTo: view.heightAnchor),
+            label.widthAnchor.constraint(equalTo: view.widthAnchor),
+            label.leftAnchor.constraint(equalTo: view.leftAnchor),
+            label.rightAnchor.constraint(equalTo: view.rightAnchor),
+        ])
+        switch component {
+            case 0:
+                label.text = "\(years[row])年"
+                label.textAlignment = .right
+            case 1:
+                label.text = "\(months[row])月"
+                label.textAlignment = .center
+            case 2:
+                label.text = "\(days[row])日"
+                label.textAlignment = .left
+            case 3:
+                label.text = "\(hours[row])時間"
+                label.textAlignment = .center
+            case 4:
+                label.text = "\(minutes[row])分"
+                label.textAlignment = .left
+            default: fatalError()
+        }
+        return view
     }
     
 }
