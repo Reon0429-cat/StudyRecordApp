1c30f124bcad933bc56b3c6160936bdf3e093d71
0429oonishi <oonishireon110@gmail.com>
2021-08-14 14:36:28 +0900

    ScreenTransitionCollectionViewCell作成

---
+++ StudyRecordApp/Views/Cells/All/ScreenTransitionCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTransitionCollectionViewCell.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/08/14.
+//
+
+import UIKit
+
+class ScreenTransitionCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
