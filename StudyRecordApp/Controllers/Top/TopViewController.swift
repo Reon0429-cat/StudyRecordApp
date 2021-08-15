d9448513369b7f3d35e0a28c607ce734829b6b4c
0429oonishi <oonishireon110@gmail.com>
2021-08-15 11:47:03 +0900

    TopVC作成

---
+++ StudyRecordApp/Controllers/Top/TopViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TopViewController.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/08/14.
+//
+
+import UIKit
+
+class TopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
