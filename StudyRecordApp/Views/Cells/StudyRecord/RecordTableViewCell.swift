c7f6e9ebd7af3ed0bba695e4dee1ca601925f702
0429oonishi <oonishireon110@gmail.com>
2021-08-19 13:15:05 +0900

    RecordTableViewCell作成

---
+++ StudyRecordApp/Views/Cells/StudyRecord/RecordTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecordTableViewCell.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/08/19.
+//
+
+import UIKit
+
+class RecordTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
