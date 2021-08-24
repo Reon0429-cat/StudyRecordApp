c9728040737bfc07db017554640cf5f84ba5dd70
0429oonishi <oonishireon110@gmail.com>
2021-08-24 13:04:56 +0900

    static func instantiate()を共通化するコードを実装

--- StudyRecordApp/Extensions/UIKit+Extension/UIViewController+Extension.swift
+++ StudyRecordApp/Extensions/UIKit+Extension/UIViewController+Extension.swift
@@ -5,4 +5,22 @@
 //  Created by 大西玲音 on 2021/08/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    static func instantiate() -> Self {
+        var storyboardName = String(describing: self)
+        if let result = storyboardName.range(of: "ViewController") {
+            storyboardName.removeSubrange(result)
+        } else {
+            fatalError("Storyboardの名前が正しくない")
+        }
+        let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
+        let vc = storyboard.instantiateViewController(
+            identifier: String(describing: self)
+        ) as! Self
+        return vc
+    }
+    
+}
