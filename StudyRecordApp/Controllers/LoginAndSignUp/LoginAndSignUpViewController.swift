2426c77dc8c3ec4b381d7d39d8d851effb0f7f00
0429oonishi <oonishireon110@gmail.com>
2021-08-26 03:00:25 +0900

    toggleviewタップ時にcontainerViewを入れ替えた

--- StudyRecordApp/Controllers/LoginAndSignUp/LoginAndSignUpViewController.swift
+++ StudyRecordApp/Controllers/LoginAndSignUp/LoginAndSignUpViewController.swift
@@ -7,23 +7,91 @@
 
 import UIKit
 
-class LoginAndSignUpViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class LoginAndSignUpViewController: UIViewController {
+    
+    @IBOutlet private weak var loginContainerView: UIView!
+    @IBOutlet private weak var signUpContainerView: UIView!
+    @IBOutlet private weak var loginToggleView: UIView!
+    @IBOutlet private weak var signUpToggleView: UIView!
+    @IBOutlet private weak var loginLabel: UILabel!
+    @IBOutlet private weak var signUpLabel: UILabel!
+    @IBOutlet private weak var containerView: UIView!
+    
+    private enum ViewType {
+        case login
+        case signUp
+    }
+    private var viewType: ViewType = .login {
+        didSet {
+            bringContainerView()
+            setToggleViewColor()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewType = .login
+        setupToggleViews()
+        
     }
-    */
+    
+    private func bringContainerView() {
+        switch viewType {
+            case .login:
+                containerView.bringSubviewToFront(loginContainerView)
+            case .signUp:
+                containerView.bringSubviewToFront(signUpContainerView)
+        }
+    }
+    
+    private func setToggleViewColor() {
+        switch viewType {
+            case .login:
+                loginToggleView.backgroundColor = .systemRed
+                signUpToggleView.backgroundColor = .clear
+                loginLabel.textColor = .black
+                signUpLabel.textColor = .gray
+            case .signUp:
+                loginToggleView.backgroundColor = .clear
+                signUpToggleView.backgroundColor = .systemGreen
+                loginLabel.textColor = .gray
+                signUpLabel.textColor = .black
+        }
+    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        switch viewType {
+//            case .login:
+//                let loginVC = segue.destination as! LoginViewController
+//
+//            case .signUp:
+//                let signUpVC = segue.destination as! SignUpViewController
+//        }
+//    }
+    
+}
 
+// MARK: - setup
+private extension LoginAndSignUpViewController {
+    
+    func setupToggleViews() {
+        let loginTapGR = UITapGestureRecognizer(target: self,
+                                                action: #selector(loginBackViewDidTapped))
+        self.loginToggleView.addGestureRecognizer(loginTapGR)
+        let signUpTapGR = UITapGestureRecognizer(target: self,
+                                                 action: #selector(signUpBackViewDidTapped))
+        self.signUpToggleView.addGestureRecognizer(signUpTapGR)
+    }
+    
+    @objc
+    func loginBackViewDidTapped() {
+        viewType = .login
+    }
+    
+    @objc
+    func signUpBackViewDidTapped() {
+        viewType = .signUp
+    }
+    
 }
