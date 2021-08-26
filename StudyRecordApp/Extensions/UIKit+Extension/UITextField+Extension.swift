0692c184a88611229374309b3071cd862671795c
0429oonishi <oonishireon110@gmail.com>
2021-08-26 20:01:47 +0900

    UITextFIeldにアンダーラインをつけるメソッドを作成

--- StudyRecordApp/Extensions/UIKit+Extension/UITextField+Extension.swift
+++ StudyRecordApp/Extensions/UIKit+Extension/UITextField+Extension.swift
@@ -5,4 +5,17 @@
 //  Created by 大西玲音 on 2021/08/26.
 //
 
-import Foundation
+import UIKit
+
+extension UITextField {
+    
+    func setUnderLine() {
+        self.borderStyle = .none
+        let underline = UIView()
+        underline.frame = CGRect(x: 0, y: self.frame.height, width: self.frame.width, height: 2)
+        underline.backgroundColor = .black
+        self.addSubview(underline)
+        self.bringSubviewToFront(underline)
+    }
+    
+}
