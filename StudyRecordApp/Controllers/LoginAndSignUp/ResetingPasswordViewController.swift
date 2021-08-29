5ef9936d6cf9be8ffb7ff18617b0a414f92e9d71
Reon0429-cat <oonishireon110@gmail.com>
2021-08-29 14:53:52 +0900

    パスワードリセット用画面を作成

---
+++ StudyRecordApp/Controllers/LoginAndSignUp/ResetingPasswordViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResetingPasswordViewController.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/08/29.
+//
+
+import UIKit
+
+class ResetingPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
