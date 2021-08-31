607018f6d554bdba793fc3b9edbfd267b3cd274e
Reon0429-cat <oonishireon110@gmail.com>
2021-08-31 17:21:36 +0900

    GoalTableViewCell作成

---
+++ StudyRecordApp/Views/Cells/Goal/GoalTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GoalTableViewCell.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/08/31.
+//
+
+import UIKit
+
+class GoalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
