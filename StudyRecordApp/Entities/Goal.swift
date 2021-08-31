3e93809563dd2a4a1b71650d5d478af767a89363
Reon0429-cat <oonishireon110@gmail.com>
2021-08-31 16:03:01 +0900

    Goalモデルを作成

--- StudyRecordApp/Entities/Goal.swift
+++ StudyRecordApp/Entities/Goal.swift
@@ -5,7 +5,7 @@
 //  Created by 大西玲音 on 2021/08/31.
 //
 
-import UIKit
+import RealmSwift
 
 // 共通の型
 struct Goal {
@@ -16,6 +16,10 @@ struct Goal {
     var image: UIImage
     var dueDate: Date
     var createdDate: Date
+}
+
+struct Category {
+    
 }
 
 struct Priority {
@@ -46,3 +50,37 @@ enum PriorityNumber: Int, CaseIterable {
 }
 
 // Realmに依存した型
+final class GoalRealm: Object {
+    @objc dynamic var title: String = ""
+    @objc dynamic var category: RealmCategory = RealmCategory()
+    @objc dynamic var memo: String = ""
+    @objc dynamic var priority: RealmPriority = RealmPriority()
+    @objc dynamic var image: UIImage = UIImage()
+    @objc dynamic var dueDate: Date = Date()
+    @objc dynamic var createdDate: Date = Date()
+}
+
+final class RealmCategory: Object {
+    
+}
+
+final class RealmPriority: Object {
+    @objc dynamic private var markRawValue = 0
+    var mark: PriorityMark? {
+        get {
+            return PriorityMark(rawValue: markRawValue)
+        }
+        set {
+            markRawValue = newValue?.rawValue ?? 0
+        }
+    }
+    @objc dynamic private var numberRawValue = 0
+    var number: PriorityNumber? {
+        get {
+            return PriorityNumber(rawValue: numberRawValue)
+        }
+        set {
+            numberRawValue = newValue?.rawValue ?? 0
+        }
+    }
+}
