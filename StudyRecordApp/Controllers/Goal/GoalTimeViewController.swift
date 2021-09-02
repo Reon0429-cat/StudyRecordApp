548c4f5b1f72e70960ef0e039270fcb4a027b440
Reon0429-cat <oonishireon110@gmail.com>
2021-09-03 02:07:41 +0900

    GoalTimeViewController作成

---
+++ StudyRecordApp/Controllers/Goal/GoalTimeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GoalTimeViewController.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/09/03.
+//
+
+import UIKit
+
+class GoalTimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
