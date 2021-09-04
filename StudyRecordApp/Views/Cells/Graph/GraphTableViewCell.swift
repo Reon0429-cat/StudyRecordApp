8599659119adc67e13f249327b52436ef896f941
Reon0429-cat <oonishireon110@gmail.com>
2021-09-04 20:49:25 +0900

    GraphTableViewCell作成

---
+++ StudyRecordApp/Views/Cells/Graph/GraphTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GraphTableViewCell.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/09/04.
+//
+
+import UIKit
+
+class GraphTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
