b27214d28c5fbcb378d28dd40e5d6c4ff2253af8
Reon0429-cat <oonishireon110@gmail.com>
2021-09-08 18:18:54 +0900

    CustomView作成

---
+++ StudyRecordApp/Views/CustomViews/CustomSlider.swift
@@ -0,0 +1,8 @@
+//
+//  CustomSlider.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/09/08.
+//
+
+import Foundation
---
+++ StudyRecordApp/Views/CustomViews/CustomSwitch.swift
@@ -0,0 +1,8 @@
+//
+//  CustomSwitch.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/09/08.
+//
+
+import Foundation
