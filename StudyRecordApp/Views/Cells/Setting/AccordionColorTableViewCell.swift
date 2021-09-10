44a88e888595078a01e6aa991e0d7e32636b1d6c
Reon0429-cat <oonishireon110@gmail.com>
2021-09-11 06:24:20 +0900

    ColorChoicesConceptVCリファクタリング

--- StudyRecordApp/Views/Cells/Setting/AccordionColorTableViewCell.swift
+++ StudyRecordApp/Views/Cells/Setting/AccordionColorTableViewCell.swift
@@ -7,46 +7,72 @@
 
 import UIKit
 
+protocol AccordionColorTableViewCellDelegate: AnyObject {
+    func tileViewDidTapped(selectedView: UIView)
+    func titleViewDidTapped(index: Int)
+}
+
 final class AccordionColorTableViewCell: UITableViewCell {
     
-    @IBOutlet private weak var leftTileView: TileView!
-    @IBOutlet private weak var centerTileView: TileView!
-    @IBOutlet private weak var rightTileView: TileView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var titleBaseView: UIView!
+    @IBOutlet private weak var stackView: UIStackView!
     
-    var onTapEvent: ((UIView) -> Void)?
+    weak var delegate: AccordionColorTableViewCellDelegate?
     
     override func prepareForReuse() {
         super.prepareForReuse()
         
-        leftTileView.layer.cornerRadius = 0
-        centerTileView.layer.cornerRadius = 0
-        rightTileView.layer.cornerRadius = 0
+        stackView.arrangedSubviews.forEach { $0.layer.cornerRadius = 0 }
         
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        leftTileView.delegate = self
-        centerTileView.delegate = self
-        rightTileView.delegate = self
+        setupStackView()
+        setupTapGR()
+        selectionStyle = .none
         
     }
     
-    func configure(colors: [UIColor],
-                   onTapEvent: @escaping ((UIView) -> Void)) {
-        self.onTapEvent = onTapEvent
-        leftTileView.backgroundColor = colors[0]
-        centerTileView.backgroundColor = colors[1]
-        rightTileView.backgroundColor = colors[2]
+    func configure(title: String, colors: [UIColor]) {
+        titleLabel.text = title
+        stackView.arrangedSubviews.enumerated()
+            .forEach { index, view in
+                view.backgroundColor = colors[index]
+            }
     }
     
 }
 
+// MARK: - TileViewDelegate
 extension AccordionColorTableViewCell: TileViewDelegate {
     
     func tileViewDidTapped(selectedView: UIView) {
-        onTapEvent?(selectedView)
+        delegate?.tileViewDidTapped(selectedView: selectedView)
+    }
+    
+}
+
+// MARK: - setup
+private extension AccordionColorTableViewCell {
+    
+    func setupStackView() {
+        stackView.arrangedSubviews
+            .map { $0 as! TileView }
+            .forEach { $0.delegate = self }
+    }
+    
+    func setupTapGR() {
+        let tapGR = UITapGestureRecognizer(target: self,
+                                           action: #selector(tapGRAction))
+        titleBaseView.addGestureRecognizer(tapGR)
+    }
+    
+    @objc
+    func tapGRAction() {
+        delegate?.titleViewDidTapped(index: self.tag)
     }
     
 }
