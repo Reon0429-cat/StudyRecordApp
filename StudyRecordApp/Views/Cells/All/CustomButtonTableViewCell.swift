d725d63693658951295ff93fa65c0446a863efbd
Reon0429-cat <oonishireon110@gmail.com>
2021-09-10 23:35:08 +0900

    カスタムビューを作成

---
+++ StudyRecordApp/Views/Cells/All/CustomButtonTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomButtonTableViewCell.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/09/10.
+//
+
+import UIKit
+
+class CustomButtonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
