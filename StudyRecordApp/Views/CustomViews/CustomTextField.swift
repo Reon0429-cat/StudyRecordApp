3bd36add522ca4c639a67ad7c0a6d7cd99a1a777
Reon0429-cat <oonishireon110@gmail.com>
2021-09-14 05:14:08 +0900

    カスタムテキストフィールドを作成

---
+++ StudyRecordApp/Views/CustomViews/CustomTextField.swift
@@ -0,0 +1,32 @@
+//
+//  CustomTextField.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/09/14.
+//
+
+import UIKit
+
+final class CustomTextField: UITextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setup()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        setup()
+        
+    }
+    
+    func setup() {
+        tintColor = .dynamicColor(light: .black,
+                                  dark: .white)
+        setUnderLine()
+    }
+    
+}
