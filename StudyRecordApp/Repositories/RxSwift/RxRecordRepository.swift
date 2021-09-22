7c91d0d427de969fc916e87244fc8318d39c9fd0
Reon0429-cat <oonishireon110@gmail.com>
2021-09-23 08:07:45 +0900

    repositoryとusecaseのRxファイル作成

---
+++ StudyRecordApp/Repositories/RxSwift/RxRecordRepository.swift
@@ -0,0 +1,8 @@
+//
+//  RxRecordRepository.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/09/23.
+//
+
+import Foundation
---
+++ StudyRecordApp/UseCases/RxSwift/RxRecordUseCase.swift
@@ -0,0 +1,8 @@
+//
+//  RxRecordUseCase.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/09/23.
+//
+
+import Foundation
