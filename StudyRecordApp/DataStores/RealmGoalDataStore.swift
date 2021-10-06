4b1377024dcc252c122147fc8abe45c009c7c14f
Reon0429-cat <oonishireon110@gmail.com>
2021-10-07 01:16:27 +0900

    GoalDataStoreリファクタリング

--- StudyRecordApp/DataStores/RealmGoalDataStore.swift
+++ StudyRecordApp/DataStores/RealmGoalDataStore.swift
@@ -5,7 +5,7 @@
 //  Created by 大西玲音 on 2021/08/31.
 //
 
-import RealmSwift
+import Foundation
 
 protocol GoalDataStoreProtocol {
     func create(category: CategoryRealm)
@@ -16,38 +16,20 @@ protocol GoalDataStoreProtocol {
 
 final class RealmGoalDataStore: GoalDataStoreProtocol {
     
-    private let realm = try! Realm()
-    private var objects: Results<CategoryRealm> {
-        realm.objects(CategoryRealm.self)
-    }
-    
     func create(category: CategoryRealm) {
-        try! realm.write {
-            realm.add(category)
-        }
+        RealmManager.create(object: category)
     }
     
     func readAll() -> [CategoryRealm] {
-        return objects.map { $0 }
+        return RealmManager.readAll(type: CategoryRealm.self)
     }
     
     func update(category: CategoryRealm) {
-        let object = realm.object(ofType: CategoryRealm.self,
-                                  forPrimaryKey: category.identifier) ?? CategoryRealm()
-        try! realm.write {
-            object.title = category.title
-            object.isExpanded = category.isExpanded
-            object.goals.removeAll()
-            object.goals.append(objectsIn: category.goals)
-        }
+        RealmManager.update(object: category)
     }
     
     func delete(category: CategoryRealm) {
-        let object = realm.object(ofType: CategoryRealm.self,
-                                  forPrimaryKey: category.identifier) ?? CategoryRealm()
-        try! realm.write {
-            realm.delete(object)
-        }
+        RealmManager.delete(object: category)
     }
     
 }
