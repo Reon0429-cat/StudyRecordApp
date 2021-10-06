163a6e8a081b0eba0382ee0fbd339272720365ae
Reon0429-cat <oonishireon110@gmail.com>
2021-10-06 23:53:28 +0900

    RealmManagerを作成

---
+++ StudyRecordApp/Entities/Realm/RealmManager.swift
@@ -0,0 +1,75 @@
+//
+//  RealmManager.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/10/06.
+//
+
+import Foundation
+import RealmSwift
+
+final class RealmManager {
+    
+    private static let realm = try! Realm()
+    
+    static func create<T: Object>(object: T) {
+        try! realm.write {
+            realm.add(object)
+        }
+    }
+    
+    static func update<T: Object>(object: T) {
+        try! realm.write {
+            realm.add(object, update: .modified)
+        }
+    }
+    
+    static func delete<T: Object>(object: T, identifier: String) {
+        let object = realm.object(ofType: T.self,
+                                  forPrimaryKey: identifier) ?? T()
+        try! realm.write {
+            realm.delete(object)
+        }
+    }
+    
+    static func readAll<T: Object>(type: T.Type) -> [T] {
+        let objects = realm.objects(T.self).sorted(byKeyPath: "order",
+                                                   ascending: true)
+        return objects.map { $0 }
+    }
+    
+    static func setupOrder<T: Object>(type: T.Type) {
+        let objects = RealmManager.readAll(type: type)
+        objects.enumerated().forEach { index, object in
+            try! realm.write {
+                object.setValue(index, forKey: "order")
+            }
+            RealmManager.update(object: object)
+        }
+    }
+    
+    static func sort<T: Object>(sourceObject: T,
+                                destinationObject: T) {
+        let sourceObjectOrder = sourceObject.value(forKey: "order") as! Int
+        let destinationObjectOrder = destinationObject.value(forKey: "order") as! Int
+        let objects = RealmManager.readAll(type: T.self)
+        try! realm.write {
+            if sourceObjectOrder < destinationObjectOrder {
+                for index in sourceObjectOrder...destinationObjectOrder {
+                    let order = objects[index].value(forKey: "order") as! Int
+                    objects[index].setValue(order - 1,
+                                            forKey: "order")
+                }
+            } else {
+                for index in destinationObjectOrder...sourceObjectOrder {
+                    let order = objects[index].value(forKey: "order") as! Int
+                    objects[index].setValue(order + 1,
+                                            forKey: "order")
+                }
+            }
+            objects[sourceObjectOrder].setValue(destinationObjectOrder,
+                                                forKey: "order")
+        }
+    }
+    
+}
