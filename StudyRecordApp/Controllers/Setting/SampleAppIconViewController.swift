91aa115e49bc5c9db62dba97e856e79fec53c867
Reon0429-cat <oonishireon110@gmail.com>
2021-10-19 05:08:23 +0900

    画像を追加

---
+++ StudyRecordApp/Controllers/Setting/SampleAppIconViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SampleAppIconViewController.swift
+//  StudyRecordApp
+//
+//  Created by 大西玲音 on 2021/10/19.
+//
+
+import UIKit
+
+final class SampleAppIconViewController: UIViewController {
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var topWaveView: WaveView!
+    @IBOutlet private weak var bottomWaveView: WaveView!
+    @IBOutlet private weak var segmentedControl: CustomSegmentedControl!
+    
+    enum ImageType: String, CaseIterable {
+        case heartWings
+        case wings
+        case wing
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        topWaveView.create(isFill: true, marginY: 50)
+        bottomWaveView.create(isFill: false, marginY: 30)
+        self.view.backgroundColor = .dynamicColor(light: .white, dark: .black)
+        imageView.tintColor = .dynamicColor(light: .black, dark: .white)
+        segmentedControl.create(ImageType.allCases.map { $0.rawValue }, selectedIndex: 0)
+        
+    }
+    
+    @IBAction private func segmentedControlDidChanged(_ sender: Any) {
+        let imageType = ImageType.allCases[segmentedControl.selectedSegmentIndex]
+        imageView.image = UIImage(named: imageType.rawValue)
+    }
+    
+}
